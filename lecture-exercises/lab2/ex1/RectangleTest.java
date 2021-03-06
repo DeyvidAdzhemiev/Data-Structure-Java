7d3a3ccd33fcc10ccc063508cf1027ab3876b207
DeyvidAdzhemiev <60064484+DeyvidAdzhemiev@users.noreply.github.com>
2021-03-06 10:18:26 +0200

    Added new file

---
+++ lecture-exercises/lab2/ex1/RectangleTest.java
@@ -0,0 +1,21 @@
+package shape;
+
+import java.util.Scanner;
+
+public class RectangleTest
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Width: ");
+        double width = input.nextDouble();
+
+        System.out.print("Enter Height: ");
+        double height = input.nextDouble();
+
+        Rectangle rect = new Rectangle(width, height);
+
+        System.out.printf("Rectangle {width: %.2f, height: %.2f, color: %s,}\nArea: %.2f\nPerimeter: %.2f",
+                rect.getWidth(), rect.getHeight(), rect.getColor(), rect.getArea(), rect.getPerimeter());
+    }
+}
